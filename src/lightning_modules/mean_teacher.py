06132d4a1ca16914816e1ccfa3b405c548004b4c
Afonso Santos <santos.osorio.afonso@gmail.com>
2025-11-17 14:09:12 +0100

    Second commit

--- src/lightning_modules/mean_teacher.py
+++ src/lightning_modules/mean_teacher.py
@@ -1,38 +1,8 @@
 import logging
-<<<<<<< HEAD
-from typing import Callable, Optional
-=======
->>>>>>> origin/main
 
 import pytorch_lightning as L
 import torch
 import torch.nn as nn
-<<<<<<< HEAD
-from torch.optim import SGD
-from torch.optim.lr_scheduler import CosineAnnealingLR, LinearLR, SequentialLR
-from torchmetrics import MetricCollection
-
-from ema_pytorch import EMA
-import torch.nn.functional as F
-
-
-from torchmetrics.classification import (
-    BinaryAccuracy,
-    BinaryAveragePrecision,
-    BinaryAUROC,
-    BinaryF1Score,
-    BinaryPrecision,
-    BinaryRecall,
-    MulticlassAccuracy,
-    MulticlassAveragePrecision,
-    MulticlassAUROC,
-    MulticlassF1Score,
-    MulticlassPrecision,
-    MulticlassRecall,
-)
-
-# Set up logging
-=======
 import torch.nn.functional as F
 import torch_geometric.transforms as T
 from torch.optim import SGD
@@ -47,23 +17,10 @@ from torchmetrics.regression import (
     R2Score,
 )
 
->>>>>>> origin/main
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-<<<<<<< HEAD
-class BaselineModel(L.LightningModule):
-    def __init__(
-        self,
-        model: nn.Module,
-        num_classes: int,
-        task_type: str = "classification",
-        learning_rate: float = 1e-3,
-        warmup_epochs: int = 5,
-        cosine_period_ratio: float = 1,
-        compile_mode: str = None,
-=======
 class MeanTeacherLoss(nn.Module):
     """
     Calculates the Mean Teacher loss based on arXiv:1703.01780v6.
@@ -108,7 +65,7 @@ class MeanTeacherLoss(nn.Module):
             return torch.tensor(self.max_weight)
         else:
             phase = 1.0 - timestep / self.steps
-            weight = self.max_weight * float(torch.exp(-5.0 * phase * phase))
+            weight = self.max_weight * float(torch.exp(torch.tensor(-5.0 * phase * phase)))
             return torch.tensor(weight)
 
     def forward(
@@ -173,44 +130,10 @@ class MeanTeacherRegressionModel(L.LightningModule):
         warmup_epochs: int = 5,
         cosine_period_ratio: float = 1,
         compile: bool = True,
->>>>>>> origin/main
         weights: str = None,
         weight_decay: float = 3e-5,
         nesterov: bool = True,
         momentum: float = 0.99,
-<<<<<<< HEAD
-        train_transforms: Optional[Callable] = None,
-        val_transforms: Optional[Callable] = None,
-        test_transforms: Optional[Callable] = None,
-        lambda_consistency: float = 1.0, 
-        ema_decay: float = 0.999,
-    ):
-        super().__init__()
-
-        # Loss function for binary and multi-class classification
-        self.loss_fn = (
-            if task_type = "regression" 
-            self.loss_fn = nn.mse_loss
-            if num_classes == 1
-            self.loss_fn = nn.BCEWithLogitsLoss() 
-            else nn.mse_loss()
-        )
-
-        # Optimizer and scheduler params
-        self.learning_rate = learning_rate
-        self.warmup_epochs = warmup_epochs
-        self.cosine_period_ratio = cosine_period_ratio
-        self.weight_decay = weight_decay
-        self.nesterov = nesterov
-        self.momentum = momentum
-
-        self.train_transforms = train_transforms
-        self.val_transforms = val_transforms
-        self.test_transforms = test_transforms
-
-        self.num_classes = num_classes
-
-=======
         ema_decay: float = 0.999,
         consistency_weight: float = 1e-1,
         consistency_rampup_epochs: int = 5,  # Constant cons weight
@@ -235,71 +158,10 @@ class MeanTeacherRegressionModel(L.LightningModule):
         # )
 
         # Metrics config
->>>>>>> origin/main
         self.train_metrics = self.configure_metrics("train")
         self.val_metrics = self.configure_metrics("val")
         self.test_metrics = self.configure_metrics("test")
 
-<<<<<<< HEAD
-        self.save_hyperparameters(ignore=["model"])
-
-        self.model = model
-        self.ema_decay = ema_decay
-        self.lambda_consistency = lambda_consistency
-
-        if weights is not None:
-            log.info(f"Loading weights from {weights}")
-            self.load_weights(weights)
-
-        self.ema = EMA(self.model, beta=self.ema_decay)
-
-        self.model = (
-            torch.compile(model, mode=compile_mode)
-            if compile_mode is not None
-            else model
-        )
-
-    def forward(self, x):
-        return self.model(x)
-
-    def training_step(self, batch, batch_idx):
-        # Graph classification step for training
-        logits = self.model(batch)
-        labels = batch.y.view(-1, self.num_classes).float()
-        # Supervised loss
-        supervised_loss = self.loss_fn(logits, labels)
-
-        #Teacher forward pass (no grad)
-        with torch.no_grad():
-            teacher_logits = self.ema.ema_model(batch)
-        # Consistency loss
-        lambda_consistency = self.lambda_consistency # weight for consistency loss
-        consistency_loss = F.mse_loss(logits, teacher_logits)
-
-        # Total loss
-        loss = supervised_loss + lambda_consistency * consistency_loss
-
-        # Update metrics with student logits
-        self.train_metrics(logits, labels)
-        self.log(
-            "train/loss",
-            loss,
-            on_step=True,
-            on_epoch=True,
-            prog_bar=True,
-            batch_size=batch.y.size(0),
-        )
-        return loss
-
-
-    def validation_step(self, batch, batch_idx):
-        logits = self.ema.ema_model(batch)
-        labels = batch.y.view(-1, self.num_classes).float()
-        loss = self.loss_fn(logits, labels)
-        self.val_metrics(logits, labels)
-        self.log(
-            "val/loss",
-=======
         # Loss config
         self.loss_fn = MeanTeacherLoss()
 
@@ -447,56 +309,15 @@ class MeanTeacherRegressionModel(L.LightningModule):
 
         self.log(
             "test/loss",
->>>>>>> origin/main
             loss,
             on_step=False,
             on_epoch=True,
             prog_bar=True,
-<<<<<<< HEAD
-            batch_size=batch.y.size(0),
-=======
->>>>>>> origin/main
         )
         return loss
 
     def configure_optimizers(self):
         optimizer = SGD(
-<<<<<<< HEAD
-            self.model.parameters(),
-            lr=self.learning_rate,
-            weight_decay=self.weight_decay,
-            momentum=self.momentum,
-            nesterov=self.nesterov,
-        )
-
-        try:
-            if self.trainer.datamodule:
-                steps_per_epoch = len(self.trainer.datamodule.train_dataloader())
-            else:
-                steps_per_epoch = len(self.trainer.train_dataloader)
-        except Exception:
-            log.warning("Could not determine steps_per_epoch. Using fallback of 1000.")
-            steps_per_epoch = 1000
-
-        if steps_per_epoch == 0:
-            log.warning("steps_per_epoch is zero. Using fallback of 1000.")
-            steps_per_epoch = 1000
-
-        total_steps = self.trainer.max_epochs * steps_per_epoch
-        warmup_steps = self.warmup_epochs * steps_per_epoch
-        cosine_steps = max(
-            1, int(self.cosine_period_ratio * (total_steps - warmup_steps))
-        )
-
-        log.info(f"Optimizer: SGD, LR: {self.learning_rate}")
-        log.info(
-            f"Total steps: {total_steps}, Warmup steps: {warmup_steps}, Cosine steps: {cosine_steps}"
-        )
-
-        cosine_scheduler = CosineAnnealingLR(optimizer, T_max=cosine_steps)
-
-        if self.warmup_epochs > 0 and warmup_steps > 0:
-=======
             self.student_model.parameters(),
             lr=self.hparams.learning_rate,
             weight_decay=self.hparams.weight_decay,
@@ -514,7 +335,6 @@ class MeanTeacherRegressionModel(L.LightningModule):
         cosine_scheduler = CosineAnnealingLR(optimizer, T_max=cosine_steps)
 
         if self.hparams.warmup_epochs > 0 and warmup_steps > 0:
->>>>>>> origin/main
             warmup_scheduler = LinearLR(
                 optimizer,
                 start_factor=1.0 / 1000,
@@ -537,65 +357,6 @@ class MeanTeacherRegressionModel(L.LightningModule):
         return [optimizer], [scheduler_config]
 
     def configure_metrics(self, prefix: str):
-<<<<<<< HEAD
-        # Binary classification metrics
-        if self.num_classes == 1:
-            return MetricCollection(
-                {
-                    f"{prefix}/avg_precision": BinaryAveragePrecision(),
-                    f"{prefix}/accuracy": BinaryAccuracy(),
-                    f"{prefix}/precision": BinaryPrecision(),
-                    f"{prefix}/recall": BinaryRecall(),
-                    f"{prefix}/f1": BinaryF1Score(),
-                    f"{prefix}/rocauc": BinaryAUROC(),
-                }
-            )
-        # Multi-class classification metrics
-        elif self.num_classes > 1:
-            return MetricCollection(
-                {
-                    f"{prefix}/avg_precision": MulticlassAveragePrecision(
-                        num_classes=self.num_classes, average="macro"
-                    ),
-                    f"{prefix}/accuracy": MulticlassAccuracy(
-                        num_classes=self.num_classes, average="macro"
-                    ),
-                    f"{prefix}/precision": MulticlassPrecision(
-                        num_classes=self.num_classes, average="macro"
-                    ),
-                    f"{prefix}/recall": MulticlassRecall(
-                        num_classes=self.num_classes, average="macro"
-                    ),
-                    f"{prefix}/f1": MulticlassF1Score(
-                        num_classes=self.num_classes, average="macro"
-                    ),
-                    f"{prefix}/rocauc": MulticlassAUROC(
-                        num_classes=self.num_classes, average="macro"
-                    ),
-                }
-            )
-        else:
-            raise ValueError("num_classes must be at least 1.")
-
-    def on_after_batch_transfer(self, batch, dataloader_idx):
-        """
-        Applies transforms to the batch *after* it has been moved to the
-        target device (e.g., GPU).
-        """
-        if self.trainer.training and self.train_transforms is not None:
-            batch = self.train_transforms(batch)
-        elif (
-            self.trainer.validating or self.trainer.sanity_checking
-        ) and self.val_transforms is not None:
-            batch = self.val_transforms(batch)
-        elif (
-            self.trainer.testing or self.trainer.predicting
-        ) and self.test_transforms is not None:
-            batch = self.test_transforms(batch)
-        return batch
-
-    def on_train_epoch_end(self):
-=======
         """Configure regression metrics for training, validation, and testing."""
         return MetricCollection(
             {
@@ -607,7 +368,6 @@ class MeanTeacherRegressionModel(L.LightningModule):
 
     def on_train_epoch_end(self):
         """Log training metrics at the end of training epoch."""
->>>>>>> origin/main
         if self.train_metrics:
             metrics = self.train_metrics.compute()
             self.log_dict(
@@ -615,16 +375,8 @@ class MeanTeacherRegressionModel(L.LightningModule):
             )
             self.train_metrics.reset()
 
-<<<<<<< HEAD
-    def on_train_batch_end(self, outputs, batch, batch_idx):
-        # Update EMA weights after each training step
-        self.ema.update()
-
-    def on_validation_epoch_end(self):
-=======
     def on_validation_epoch_end(self):
         """Log validation metrics at the end of validation epoch."""
->>>>>>> origin/main
         if self.val_metrics:
             metrics = self.val_metrics.compute()
             self.log_dict(
@@ -633,10 +385,7 @@ class MeanTeacherRegressionModel(L.LightningModule):
             self.val_metrics.reset()
 
     def on_test_epoch_end(self):
-<<<<<<< HEAD
-=======
         """Log test metrics at the end of testing epoch."""
->>>>>>> origin/main
         if self.test_metrics:
             metrics = self.test_metrics.compute()
             self.log_dict(
@@ -645,73 +394,6 @@ class MeanTeacherRegressionModel(L.LightningModule):
             self.test_metrics.reset()
 
     def load_weights(self, weights_path: str):
-<<<<<<< HEAD
-        state_dict = torch.load(weights_path, map_location="cpu")
-        self.model.load_state_dict(state_dict)
-
-        log.info(f"Weights loaded from {weights_path}")
-
-
-if __name__ == "__main__":
-    from src.models.gcn import GCN
-    from src.data.qm9 import QM9DataModule
-    from src.data.moleculenet import MoleculeNetDataModule
-    from pytorch_lightning.callbacks import TQDMProgressBar, ModelCheckpoint
-    from pytorch_lightning import Trainer
-    from pytorch_lightning.loggers import WandbLogger
-    import wandb
-
-    # data_module = QM9DataModule(
-    #     target=0,
-    #     batch_size_train=32,
-    #     batch_size_inference=32,
-    #     num_workers=1,
-    # )
-    data_module = QM9DataModule( # if was to use another dataset, I would have to add the name parameter (moleculenet)
-        target=4,  
-        batch_size_train=256,
-        batch_size_inference=256,
-        num_workers=4,
-        
-    )
-    data_module.prepare_data()
-    data_module.setup()
-
-    baseline_module = BaselineModel(
-        model=GCN(num_node_features=data_module.num_features, hidden_channels=64),
-        num_classes=data_module.num_classes,
-        lambda_consistency=1.0,
-        ema_decay=0.999,
-    )
-
-    wandb_logger = WandbLogger(
-        project="semi-supervised-gnn",
-        name="mean-teacher-qm9",
-        log_model=True,
-        save_dir="./logs",
-    )
-
-    # Watch the model to log gradients and parameters
-    wandb_logger.watch(baseline_module.model, log="all", log_freq=100)
-
-    # Add checkpoint callback
-    checkpoint_callback = ModelCheckpoint(
-        monitor="val/loss",
-        dirpath="./checkpoints",
-        filename="mean-teacher-qm9-{epoch:02d}-{val/loss:.2f}",
-        save_top_k=3,
-        mode="min",
-    )
-
-    # Run trainer in debug mode fast_dev_run=True,
-    trainer = Trainer(max_epochs=100,
-     callbacks=[TQDMProgressBar(), checkpoint_callback],
-      logger=wandb_logger,
-      log_every_n_steps=10,
-      enable_progress_bar=True,
-      enable_model_summary=True,)
-    trainer.fit(baseline_module, datamodule=data_module)
-=======
         """Load pretrained weights into both student and teacher models."""
         state_dict = torch.load(weights_path, map_location="cpu")
         self.student_model.load_state_dict(state_dict)
@@ -771,6 +453,9 @@ if __name__ == "__main__":
         batch_size_inference=256,
         num_workers=4,
         target=0,
+        #mode = "semi_supervised",
+        subset_size=5000,
+        splits=(0.5, 0.3, 0.1, 0.1), # experiment with different splits
     )
 
     model_name = "GIN"
@@ -791,13 +476,14 @@ if __name__ == "__main__":
     mean_teacher_model = MeanTeacherRegressionModel(
         model=model,
         num_outputs=1,
-        learning_rate=0.01,
+        learning_rate=0.001,
+        consistency_rampup_epochs=10, # added this
         augmentations=True,
         compile=True,
     )
 
     trainer = Trainer(
-        max_epochs=150,
+        max_epochs=300, # double the epochs
         accelerator="auto",
         devices="auto",
         precision="16-mixed" if torch.cuda.is_available() else "32-true",
@@ -805,4 +491,3 @@ if __name__ == "__main__":
     )
 
     trainer.fit(model=mean_teacher_model, datamodule=data_module)
->>>>>>> origin/main
